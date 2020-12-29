ee06729ea34208c7ac9e5eca9472073e6fe073b3
tsaizhenya <tsai@caizhenyadeMacBook-Pro.local>
2020-12-29 13:53:24 +0800

    first commit

---
+++ src/main/java/com/crgt/open/traincore/TrainCoreApplication.java
@@ -0,0 +1,13 @@
+package com.crgt.open.traincore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crgt/open/traincore/TrainCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crgt.open.traincore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
